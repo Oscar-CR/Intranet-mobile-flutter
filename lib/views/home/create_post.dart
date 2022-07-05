5b93ee41b1a3f5c1515949cbbb9f95048689757c
Oscar-CR <krz.oficial@gmail.com>
2022-07-05 11:09:45 -0500

    avance POST imagenes

--- lib/views/home/create_post.dart
+++ lib/views/home/create_post.dart
@@ -198,3 +198,21 @@ class _HomeState extends State<CreatePostPage> {
             )));
   }
 }
+
+
+_asyncFileUpload(String text, File file) async{
+   //create multipart request for POST or PATCH method
+   var request = http.MultipartRequest("POST", Uri.parse("<url>"));
+   //add text fields
+   request.fields["text_field"] = text;
+   //create multipart using filepath, string or bytes
+   var pic = await http.MultipartFile.fromPath("file_field", file.path);
+   //add multipart to request
+   request.files.add(pic);
+   var response = await request.send();
+
+   //Get the response from the server
+   var responseData = await response.stream.toBytes();
+   var responseString = String.fromCharCodes(responseData);
+   print(responseString);
+}
